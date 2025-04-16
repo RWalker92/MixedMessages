645e5b77e7254265a5aca8853dfd8bec1f4fd08b
RWalker92 <ryan.walker1992@gmail.com>
2025-04-16 18:13:13 +0100

    core function for generating a message complete

--- main.js
+++ main.js
@@ -7,3 +7,13 @@ messages = {
     fortune: ["Good luck", "Bad luck", "Neutral luck", "Great fortune"],
     advice: ["Stay positive", "Be cautious", "Take risks", "Embrace change"],
 }
+
+function getFortune() {
+    let sign = messages.sign[generateRandomNumber(messages.sign.length)];
+    let fortune = messages.fortune[generateRandomNumber(messages.fortune.length)];
+    let advice = messages.advice[generateRandomNumber(messages.advice.length)];
+
+    return `Your sign is: ${sign}. Your fortune is: ${fortune}. Advice: ${advice}.`;
+}
+
+console.log(getFortune());
